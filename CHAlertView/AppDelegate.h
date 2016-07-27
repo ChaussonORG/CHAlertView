7221fd82ff6226b7e3fbfcc1f28be09d2a4d8765
guojintao <qaz841301847@qq.com>
2016-07-27 13:25:54 +0800

    初始化创建CHAlertView

---
+++ CHAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CHAlertView
+//
+//  Created by 郭金涛 on 16/7/27.
+//  Copyright © 2016年 Gjt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CHAlertView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CHAlertView
+//
+//  Created by 郭金涛 on 16/7/27.
+//  Copyright © 2016年 Gjt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
